05e5f2c91966c1596340f38ce8a4cf670b4cd834
lucaspg <l.paulon97@gmail.com>
2025-04-06 23:19:25 -0300

    Added JEI compatibility

---
+++ src/main/java/io/github/lucaspg/tutorialmod/compat/GrowthChamberRecipeCategory.java
@@ -0,0 +1,60 @@
+package io.github.lucaspg.tutorialmod.compat;
+
+import io.github.lucaspg.tutorialmod.TutorialMod;
+import io.github.lucaspg.tutorialmod.block.ModBlocks;
+import io.github.lucaspg.tutorialmod.recipe.GrowthChamberRecipe;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+public class GrowthChamberRecipeCategory implements IRecipeCategory<GrowthChamberRecipe> {
+    public static final ResourceLocation UID = ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, "growth_chamber");
+    public static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID,
+            "textures/gui/growth_chamber/growth_chamber_gui.png");
+
+    public static final RecipeType<GrowthChamberRecipe> GROWTH_CHAMBER_RECIPE_RECIPE_TYPE =
+            new RecipeType<>(UID, GrowthChamberRecipe.class);
+
+    private final IDrawable background;
+    private final IDrawable icon;
+
+    public GrowthChamberRecipeCategory(IGuiHelper helper) {
+        this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 85);
+        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.GROWTH_CHAMBER));
+    }
+
+    @Override
+    public RecipeType<GrowthChamberRecipe> getRecipeType() {
+        return GROWTH_CHAMBER_RECIPE_RECIPE_TYPE;
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.translatable("block.tutorialmod.growth_chamber");
+    }
+
+    @Override
+    public @Nullable IDrawable getIcon() {
+        return icon;
+    }
+
+    @Override
+    public @Nullable IDrawable getBackground() {
+        return background;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, GrowthChamberRecipe recipe, IFocusGroup focuses) {
+        builder.addSlot(RecipeIngredientRole.INPUT, 54, 34).addIngredients(recipe.getIngredients().get(0));
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 104, 34).addItemStack(recipe.getResultItem(null));
+    }
+}
